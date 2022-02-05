0de665f2c51c45ef4981f5f329686eb9c018d343
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-02-05 18:44:39 +0300

    finished brand component added brands images and create style sheet with gpt3__brand-container and styles dev for each brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
-  return <div>Brand component</div>;
+  return (
+    <div className="gpt3__brand-container section__padding" id="brand">
+      <div>
+        <img src={google} alt="Google" />
+      </div>
+      <div>
+        <img src={slack} alt="Slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="Shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
