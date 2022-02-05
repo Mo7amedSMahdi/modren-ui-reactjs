7f67959cdeda7200c56e173c2280f4a3b3b07dfd
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-02-05 19:50:35 +0300

    section whatgpt3 add section content and stylesheet for the section with heading and features class

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./whatgpt3.css";
+import Feature from "../../components/feature/Feature";
 
 const WhatGPT3 = () => {
-  return <div></div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-headding">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-features_container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
