48d695dfede6f57658b531229d918ef58d85cbd8
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-02-06 15:11:40 +0300

    create feature component with style sheet and add to accept props

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature component</div>;
+const Feature = ({ title, content }) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+
+      <div className="gpt3__features-container__feature-content">
+        <p>{content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,10 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature
+          title="What is GPT-3"
+          content="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
       </div>
       <div className="gpt3__whatgpt3-headding">
         <h1 className="gradient__text">
