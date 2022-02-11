f9e6fb35531751f86affbc2f9ca2badc4cd12afd
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-02-11 17:51:13 +0300

    making a confliect

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -12,23 +12,6 @@ const NavBar = () => {
         <div className="gpt3__navbar-links_logo">
           <img src={LogoImag} alt="logo" />
         </div>
-        <div className="gpt3__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#wgpt3">What is GPT3?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
-        </div>
       </div>
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
